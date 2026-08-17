b4e6425c5ddbb77278b6b392fecb4566c9ecc9c5
Oliver Kendell <oliver.a.kendell@gmail.com>
2026-08-17 19:56:12 +0100

    Initial password length checker.py

--- src/password_auditor.py
+++ src/password_auditor.py
@@ -1,1 +1,11 @@
-
+password = input("Input your password")
+passLength = len(password)
+print(passLength)
+if passLength < 8:
+    print("Weak Password")
+elif passLength < 11:
+    print ("Moderate Password")
+elif passLength > 12:
+    print("Strong Password")
+else:
+    quit()
