953560c7feb18dcc83056a1827649d6c8ac6b1ad
Oliver Kendell <oliver.a.kendell@gmail.com>
2026-08-17 20:05:24 +0100

    Fixed Logic, Made more "Tool Like" and professional.py
    I wanted the parameters to be
    > 8 = WEAK
    <=11 = MODERATE
    >= 12 = STRONG
    before the change it would think a password of 12 characters isn't strong, when I wanted it to think 12 characters was strong.
    
    I also made the responses more "Tool Like" so its more like a professional Password auditing tool.
    
    I also changed the input() to use pythons getpass so the password isn't displayed while being typed, making it more secure.

--- src/password_auditor.py
+++ src/password_auditor.py
@@ -1,11 +1,13 @@
-password = input("Input your password")
-passLength = len(password)
-print(passLength)
-if passLength < 8:
-    print("Weak Password")
-elif passLength < 11:
-    print ("Moderate Password")
-elif passLength > 12:
-    print("Strong Password")
+import getpass
+
+password = getpass.getpass("Input your password:\n")
+password_length = len(password)
+print(password_length)
+if password_length < 8:
+    print("WEAK")
+elif password_length <= 11:
+    print ("MODERATE")
+elif password_length >= 12:
+    print("STRONG")
 else:
     quit()
